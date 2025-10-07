3b6e64dc8cc89d9177e16a4f819266eb7c2c21e6
Marcelo Alves Cardoso Filho <marcelo.filho@lojasrenner.com.br>
2025-10-07 08:49:49 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Infrastructure as Code
+
+My personal repository containing some IaC. Feel free to copy it as many times you need.
+
+This repository contains an Ansible and Terraform files for `ubuntu-noble` virtual machine on QEMU/Libvirt.
---
+++ vms/ubuntu-noble/terraform/main.tf
@@ -0,0 +1,107 @@
+terraform {
+  required_providers {
+    libvirt = {
+      source  = "dmacvicar/libvirt"
+    }
+  }
+}
+
+provider "libvirt" {
+  uri             = "qemu:///system"
+}
+
+# VM name and resources
+variable "vm_name" {
+  type            = string
+  default         = "ubuntu-2404"
+}
+
+# Create storage volume (copy-on-write)
+resource "libvirt_volume" "ubuntu_disk" {
+  name            = "${var.vm_name}.qcow2"
+  base_volume_id  = libvirt_volume.ubuntu_base.id
+  pool            = "default"
+  format          = "qcow2"
+  size            = 42949672960
+}
+
+# Base image volume
+resource "libvirt_volume" "ubuntu_base" {
+  name            = var.vm_name
+  source          = "https://cloud-images.ubuntu.com/noble/current/noble-server-cloudimg-amd64.img"
+  pool            = "default"
+  format          = "qcow2"
+}
+
+# Cloud-init ISO to inject user-data
+resource "libvirt_cloudinit_disk" "commoninit" {
+  name            = "${var.vm_name}-cloudinit.iso"
+  user_data       = data.template_file.user_data.rendered
+  pool            = "default"
+}
+
+# Minimal user data for cloud-init setup
+data "template_file" "user_data" {
+  template        = <<EOF
+#cloud-config
+hostname: ${var.vm_name}
+
+ssh_pwauth: false
+allow_public_ssh_keys: true
+
+users:
+  - name: ubuntu
+    shell: /bin/bash
+    sudo: "ALL=(ALL) NOPASSWD:ALL"
+    groups: users, admin
+    lock_passwd: false
+    passwd: $6$hoEyHGCs56ArGwAK$XTGggRyGn97UaP1SmRQVYXz/bcyUhxYRVLomV101w0SWJKF2TqpCpeQvyPmqcEHyzNgNPsdfVm2Q8q.iIIOiY.
+    ssh_authorized_keys: 
+      - ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHhDDEDtMZlS6WijXJ4Fd9fH7i7e6Gd0IZGzIztC5cTE alvesmarcelocf@proton.me
+
+package_update: true
+package_upgrade: true
+EOF
+}
+
+# Define the VM domain
+resource "libvirt_domain" "ubuntu" {
+  name            = var.vm_name
+  memory          = 4096
+  vcpu            = 2
+  cpu {
+    mode          = "host-passthrough"
+  }
+
+  running         = false
+  cloudinit       = libvirt_cloudinit_disk.commoninit.id
+
+  disk {
+    volume_id     = libvirt_volume.ubuntu_disk.id
+  }
+
+  network_interface {
+    network_name  = "default"
+    addresses     = [ "192.168.122.100" ]
+  }
+
+  console {
+    type          = "pty"
+    target_port   = "0"
+    target_type   = "serial"
+  }
+
+  graphics {
+    type          = "spice"
+    listen_type   = "none"
+    autoport      = true
+  }
+
+  video {
+    type          = "virtio"
+  }
+
+  boot_device {
+    dev           = ["hd"]
+  }
+}
